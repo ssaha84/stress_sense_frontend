7e9187b945547fa9e319e6666fc2bdf31581bceb
sanjib saha <dr.saha.sanjib@gmail.com>
2025-09-11 17:02:43 +0200

    button is centered

--- app.py
+++ app.py
@@ -88,49 +88,6 @@ def make_pieplot(themes:list[list], slice: int = 3 )-> None:
 
     fig.update_layout(showlegend=False, margin=dict(l=0, r=0, t=0, b=0))
     st.plotly_chart(fig, use_container_width=True)
-    # '''make a pie plot if more than 1 theme was detected'''
-    # top_themes = themes[:slice]
-    # # print(top_themes)
-    # # themes_df = pd.DataFrame(top_themes, columns=["theme, confidence"])
-    # # print(themes_df.columns)
-    # # print(themes_df)
-
-    # themes_df = pd.DataFrame(top_themes)
-    # colors = ["#CAE0F8",  # powder blue
-    # "#F7E2B6",  # beige
-    # "#D7F7D7"   # peach cream
-    # ]
-
-    # # themes_df.rename(columns={0: "theme", 1: "confidence"})
-    # fig = px.pie(themes_df, values=1, names=0, category_orders={1: top_themes})
-
-    # largest_idx = themes_df[1].idxmax()
-    # pull = [0.05 if i == largest_idx else 0 for i in range(len(themes_df))]
-
-    # max_val = themes_df[1].max()
-    # templates = [
-    # "<b>%{label}</b><br><b>%{percent}</b>" if v == max_val else "%{label}<br>%{percent}"
-    # for v in themes_df[1]]
-
-    # fig.update_traces(
-    #     pull=pull,
-    #     textinfo="label+percent",   # show names (“titles”) and % inside
-    #     textposition="inside",
-    #     insidetextorientation="horizontal",
-    #     texttemplate=templates,      # per-slice templates
-    #     textfont_size=14,
-    #     marker=dict(
-    #         colors=colors,
-    #         line=dict(color="white", width=2)  # crisp slice borders
-    #     ),
-    #     rotation=180,
-    #     direction="clockwise"
-    # )
-
-    # fig.update_layout(showlegend=False)  # labels are already inside
-    # st.plotly_chart(fig, use_container_width=True)
-
-
 
 #load_dotenv() # Load environment variables from .env file
 
@@ -148,7 +105,7 @@ st.markdown("### What's on your mind? What's going on in your life?")
 
 prompt = st.text_area(" ").strip()
 
-if st.button("Spot the Stress"):
+if st.button("Spot the Stress", use_container_width=True):
 
     if len(prompt) == 0:
         st.warning('Write something about what you are feeling right now.')
